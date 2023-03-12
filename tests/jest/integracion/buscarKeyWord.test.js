ebf626a3c4826bfbe652b12c0b6acbb93c4b910e
Ángel <angelphi1618@gmail.com>
2023-03-12 18:25:06 +0100

    Test GitHub Actions

--- tests/jest/integracion/buscarKeyWord.test.js
+++ tests/jest/integracion/buscarKeyWord.test.js
@@ -12,6 +12,20 @@ describe('Test de Integración búsqueda de película por keyword', () => {
 	
 	// Creamos una película antes de todo
 	beforeAll(async () => {
+		await dao.query(`CREATE TABLE peliculas (
+							id int(11) NOT NULL,
+							nombre varchar(30) NOT NULL,
+							img mediumblob NOT NULL,
+							duracion int(11) NOT NULL,
+							puntuacion int(11) NOT NULL,
+							fechaEstreno date NOT NULL,
+							sinopsis text NOT NULL,
+							genero varchar(32) NOT NULL
+						)`);
+		
+		await dao.query(`ALTER TABLE peliculas ADD PRIMARY KEY (id)`);
+		await dao.query(`ALTER TABLE peliculas MODIFY id int(11) NOT NULL AUTO_INCREMENT`)
+		
 		await dao.query("DELETE FROM peliculas WHERE id>0;");
 		await dao.createFilm('Alien: el octavo pasajero', 1, 115, 9, '1979-05-25', 'La tripulación del remolcador espacial Nostromo atiende una señal de socorro y, sin saberlo, sube a bordo una letal forma de vida extraterrestre.', 'Terror');
 	});
