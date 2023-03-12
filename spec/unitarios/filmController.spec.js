334ac3d9c281f86f4cc5fcce6f0291a5f45909cf
oreterraC <diegolanseiros@gmail.com>
2023-03-12 18:34:36 +0100

    Pruebas con Jasmine

---
+++ spec/unitarios/filmController.spec.js
@@ -0,0 +1,58 @@
+var assert = require('assert');
+
+const FilmDAO = require('../../tests/stubs/filmDAOstub');
+
+const FilmController = require('../../controller/filmController');
+const sinon = require("sinon");
+
+const views = require("../../js/configView");
+
+const peliculas = [
+    { nombre: "Alien", img: 1 }, 
+    { nombre: "Terminator", img: 1 },
+    { nombre: "Shrek", img: 1 }
+];
+
+const dao = new FilmDAO(peliculas);
+const filmController = new FilmController(dao);
+
+const req = {
+    body: {},
+    query: {},
+    params: {}
+};
+
+const res = {
+    send: sinon.spy(),
+    json: sinon.spy(),
+    render: sinon.spy()
+};
+
+describe('Test Controlador Películas', () => {
+
+    it('Probando el retorno de la función postListByKeyWord', async function(){
+        req.body.nombreBuscar = "Alien";
+
+		await filmController.postListByKeyWord(req, res);
+        
+        sinon.assert.calledWithMatch(res.render, views.index , {
+            title: 'Mostrando resultados',
+            films: [
+                peliculas[0]
+            ]
+        })
+    });
+
+    it('Búsqueda de películas por palabra clave cuando la peli NO existe', async () => {
+		req.body.nombreBuscar = "pelicula que no existe";
+
+        await filmController.postListByKeyWord(req, res);
+
+        sinon.assert.calledWithMatch(res.render, views.index , {
+            title: 'Mostrando resultados',
+            films: [ ]
+        });
+        
+	});
+    
+});
