32eb29008000e64b5c85fd142eb611cfb51777a6
andre Aylas Hernandez <oaylash@gmail.com>
2023-03-19 12:08:25 +0100

    cambios de pruebas mostrar informacion

--- tests/jest/unitarios/filmController.test.js
+++ tests/jest/unitarios/filmController.test.js
@@ -1,5 +1,6 @@
 const FilmDAO = require('../../stubs/filmDAOstub');
 
+
 const FilmController = require('../../../controller/filmController');
 
 const peliculas = [{
@@ -11,6 +12,43 @@ const peliculas = [{
 }, {
 	nombre: "Shrek",
 	img: 1
+}];
+
+const infoBasic=[{
+	id: 1,
+	nombre:	"Terminator",
+	img: 1,
+	duracion: 108,
+	puntuacion: 10,
+	fechaEstreno: "1984-10-26",
+	sinopsis: "En el año 2029 las máquinas dominan el mundo. Los rebeldes que luchan contra ellas tienen como líder a John Connor, un hombre que nació en los años ochenta. Para eliminarlo y así acabar con la rebelión, las máquinas envían al pasado el robot Terminator con la misión de matar a Sarah Connor, la madre de John, e impedir así su nacimiento. Sin embargo, un hombre del futuro intentará protegerla.",
+	genero: "Ciencia ficción",
+	nombreAct:"Arnold",
+	apellidosAct: "Schwarzenegger"
+},
+{
+	id: 1,
+	nombre:	"Terminator",
+	img: 1,
+	duracion: 108,
+	puntuacion: 10,
+	fechaEstreno: "1984-10-26",
+	sinopsis: "En el año 2029 las máquinas dominan el mundo. Los rebeldes que luchan contra ellas tienen como líder a John Connor, un hombre que nació en los años ochenta. Para eliminarlo y así acabar con la rebelión, las máquinas envían al pasado el robot Terminator con la misión de matar a Sarah Connor, la madre de John, e impedir así su nacimiento. Sin embargo, un hombre del futuro intentará protegerla.",
+	genero: "Ciencia ficción",
+	nombreAct:"Linda",
+	apellidosAct: "Hamilton"
+},
+{
+	id: 1,
+	nombre:	"Terminator",
+	img: 1,
+	duracion: 108,
+	puntuacion: 10,
+	fechaEstreno: "1984-10-26",
+	sinopsis: "En el año 2029 las máquinas dominan el mundo. Los rebeldes que luchan contra ellas tienen como líder a John Connor, un hombre que nació en los años ochenta. Para eliminarlo y así acabar con la rebelión, las máquinas envían al pasado el robot Terminator con la misión de matar a Sarah Connor, la madre de John, e impedir así su nacimiento. Sin embargo, un hombre del futuro intentará protegerla.",
+	genero: "Ciencia ficción",
+	nombreAct:"Michael",
+	apellidosAct: "Biehn"
 }];
 
 
@@ -52,5 +90,48 @@ describe('Test Controlador Películas', () => {
 		expect(res.render).toHaveBeenCalledWith(expect.anything(), expect.objectContaining({
 			films: expect.arrayContaining([ ])
 		}));
+	});
+});
+	//dao.basicInfoFilms(req.body.)
+
+
+describe('Test Controlador de datos basicos...',()=>{
+	const dao = new FilmDAO(infoBasic);
+	const filmController = new FilmController(dao);
+
+	const req = {
+		body: {},
+		query: {},
+		params: {}
+	};
+
+	const res = {
+		send: jest.fn(),
+		json: jest.fn(),
+		render: jest.fn()
+	};
+	test('Busqueda de informacion con id existente',async()=>{
+		req.body.id=1;
+
+		await filmController.getFilmByIdCtrl(req,res);
+		//esperando que funciones
+		expect(res.render).toHaveBeenCalledWith(expect.anything(),expect.objectContaining({
+			films: expect.arrayContaining([
+				infoBasic[0],
+				infoBasic[1],
+				infoBasic[2]
+			])
+		}));
+	});
+
+	test('Busqueda de informacion con id no existente',async()=>{
+		req.body.id=2;
+
+		await filmController.getFilmByIdCtrl(req,res);
+
+		expect(res.render).toHaveBeenCalledWith(expect.anything(),expect.objectContaining({
+			films: expect.arrayContaining([ ])
+		}));
+
 	});
 });
--- tests/stubs/filmDAOstub.js
+++ tests/stubs/filmDAOstub.js
@@ -1,12 +1,19 @@
 class FilmDAO {
 
 	constructor(peliculas){
-		this.lista = peliculas
+		this.lista = peliculas;
 	}
+	
 	
 	async listFilms(keyWord){
 		return this.lista.filter(p => p.nombre.includes(keyWord));
 	}
+
+	async basicInfoFilms(information){
+		return this.lista.toString()===information.toString();
+	}
+
+
 }
 
 module.exports = FilmDAO;
