70af3fcad4c67c0d06d8d17fb83a105b36447475
andre Aylas Hernandez <oaylash@gmail.com>
2023-03-21 22:17:18 +0100

    avanzado a las 9 pm

--- tests/jest/integracion/mirarInfoBasica.test.js
+++ tests/jest/integracion/mirarInfoBasica.test.js
@@ -36,47 +36,71 @@ describe('Test de integracion de la informacion basica de una pelicula',()=>{
 
 		await dao.query(`CREATE TABLE actores (
 							id int(11) NOT NULL,
-							nombre varchar(30) NOT NULL,
-							apellidos varchar(40) NOT NULL
+							nombreAct varchar(30) NOT NULL,
+							apellidosAct varchar(40) NOT NULL
 		  				)`);
 
 		await dao.query(`ALTER TABLE actores ADD PRIMARY KEY (id)`);
 
 		await dao.query(`ALTER TABLE actores
-  						MODIFY id int(11) NOT NULL AUTO_INCREMENT`);				
+  						MODIFY id int(11) NOT NULL AUTO_INCREMENT`);			
 			
-		await dao.query("DELETE FROM actores_peliculas WHERE id_actor>0 and id_pelicula>0;");
-		await dao.query("DELETE FROM actores WHERE id>0 and id>0;");
-		await dao.query("DELETE FROM peliculas WHERE id>0;");
+		//await dao.query("DELETE FROM actores_peliculas WHERE id_actor>0 and id_pelicula>0;");
+		//await dao.query("DELETE FROM actores WHERE id>0 and id>0;");
+		//await dao.query("DELETE FROM peliculas WHERE id>0;");
 		await dao.createFilm('Alien: el octavo pasajero', 1, 115, 9, '1979-05-25', 'La tripulación del remolcador espacial Nostromo atiende una señal de socorro y, sin saberlo, sube a bordo una letal forma de vida extraterrestre.', 'Terror');
-		await dao.query("INSERT INTO actores values('Sigourney','Weaver');");
-		await dao.query("INSERT INTO actores values('John','Hurt');");
-		await dao.query("INSERT INTO actores values('Ian','Holm');");
-		await dao.query("INSERT INTO actores_peliculas values(1,1)");
-		await dao.query("INSERT INTO actores_peliculas values(1,1)");
-		await dao.query("INSERT INTO actores_peliculas values(2,1)");
-		await dao.query("INSERT INTO actores_peliculas values(3,1)");
+		
+		await dao.query("INSERT INTO actores(id, nombreAct, apellidosAct) values(1,'Sigourney','Weaver');");
+		await dao.query("INSERT INTO actores(id, nombreAct, apellidosAct) values(2,'John','Hurt');");
+		await dao.query("INSERT INTO actores(id, nombreAct, apellidosAct) values(3,'Ian','Holm');");
+		await dao.query("INSERT INTO actores_peliculas(id_actor,id_pelicula) values(1,1)");
+		await dao.query("INSERT INTO actores_peliculas(id_actor,id_pelicula) values(2,1)");
+		await dao.query("INSERT INTO actores_peliculas(id_actor,id_pelicula) values(3,1)");
 
 	});
 
 	afterAll(async () => {
-		await dao.query("DELETE FROM actores_peliculas WHERE id_actores>0 and id_peliculas>0;")
+		await dao.query("DELETE FROM actores_peliculas WHERE id_actor>0 and id_pelicula>0;")
 		await dao.query("DELETE FROM actores WHERE id>0;");
 		await dao.query("DELETE FROM peliculas WHERE id>0;");
+		await dao.query("DROP TABLE actores_peliculas;");
+        await dao.query("DROP TABLE actores;");
+        await dao.query("DROP TABLE peliculas;");
 		await pool.end();
 	});
 
 
 	test('Busqueda de informacion con id existente',async()=>{
 		const id=1;
-
-		await dao.getFilmByIdCtrl(id).then(result => {
-			expect(result).toEqual(expect.arrayContaining([
+		await dao.getFilmById(id).then(result => {
+			
+			
+			expect(result).toContain([
 				expect.objectContaining({
-					titleV: "Alien: el octavo pasajero",
-					generoV: "Terror"
-				})
-			]))
+					id: 1,
+					nombre: "Alien: el octavo pasajero",
+					duracion:115,
+					puntuacion: 9,
+					fecha:"1979-05-24T22:00:00.000Z",
+					sinopsis: "La tripulación del remolcador espacial Nostromo atiende una señal de socorro y, sin saberlo, sube a bordo una letal forma de vida extraterrestre.",
+					genero:"Terror",
+					nombreAct:"Sigourney",apellidosAct:"Weaver"})
+				expect.objectContaining({id: 1,
+						nombre: "Alien: el octavo pasajero",
+						duracion:115,
+						puntuacion: 9,
+						fecha:"1979-05-24T22:00:00.000Z",
+						sinopsis: "La tripulación del remolcador espacial Nostromo atiende una señal de socorro y, sin saberlo, sube a bordo una letal forma de vida extraterrestre.",
+						genero:"Terror",
+						nombreAct:"John",apellidosAct:"Hurt"})
+				expect.objectContaining({id: 1,
+							nombre: "Alien: el octavo pasajero",
+							duracion:115,
+							puntuacion: 9,
+							fecha:"1979-05-24T22:00:00.000Z",
+							sinopsis: "La tripulación del remolcador espacial Nostromo atiende una señal de socorro y, sin saberlo, sube a bordo una letal forma de vida extraterrestre.",
+							genero:"Terror",
+							nombreAct:"Ian",apellidosAct:"Holm"});
 		});	
 			
 	});
