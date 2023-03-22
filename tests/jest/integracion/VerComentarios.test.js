b9914e4878e5b183a170868f58b5ad76e31babff
oreterraC <diegolanseiros@gmail.com>
2023-03-22 17:42:23 +0100

    Pruebas de integración de ver comentarios

--- tests/jest/integracion/VerComentarios.test.js
+++ tests/jest/integracion/VerComentarios.test.js
@@ -13,6 +13,7 @@ describe('Test de Integración ver comentarios de una película', () => {
 	
 	// Creamos una película antes de todo
 	beforeAll(async () => {
+
 		await dao.query(`CREATE TABLE peliculas (
 							id int(11) NOT NULL,
 							nombre varchar(30) NOT NULL,
@@ -26,44 +27,64 @@ describe('Test de Integración ver comentarios de una película', () => {
 		
 		await dao.query(`ALTER TABLE peliculas ADD PRIMARY KEY (id)`);
 		await dao.query(`ALTER TABLE peliculas MODIFY id int(11) NOT NULL AUTO_INCREMENT`)
-		
-		await dao.query(`CREATE TABLE actores_peliculas (
-							id_actor int(11) NOT NULL,
-							id_pelicula int(11) NOT NULL
-		  				)`);
 
 		await dao.query(`CREATE TABLE actores (
 							id int(11) NOT NULL,
 							nombre varchar(30) NOT NULL,
 							apellidos varchar(40) NOT NULL
-		  				)`)
+		  				)`);
+		
+		await dao.query(`CREATE TABLE actores_peliculas (
+							id_actor int(11) NOT NULL,
+							id_pelicula int(11) NOT NULL
+		  				)`);
+			
+		await dao.query(`CREATE TABLE usuarios (
+							id int(11) NOT NULL,
+							nombreCompleto varchar(45) NOT NULL,
+							username varchar(30) NOT NULL,
+							email varchar(255) NOT NULL,
+							password varchar(250) NOT NULL
+						)`);
+
+		await dao.query(`CREATE TABLE comentarios (
+							id int(11) PRIMARY KEY AUTO_INCREMENT,
+							id_usuario int(11) NOT NULL,
+							id_pelicula int(11) NOT NULL,
+							texto varchar(200),
+							fecha datetime
+						)`);
 
 		await dao.query("DELETE FROM peliculas WHERE id > 0;");
+
 		await dao.createFilm('Alien: el octavo pasajero', 1, 115, 9, '1979-05-25', 'La tripulación del remolcador espacial Nostromo atiende una señal de socorro y, sin saberlo, sube a bordo una letal forma de vida extraterrestre.', 'Terror');
+		await dao.createUser(1, 'Eustagelio', 'eusta', 'eustagelio@kgmail.com', 'aBcDe1*');
+		await dao.creatComment(1, 1, 1, 'Amo a mi mamá', '1975-05-25');
+	
 	});
 
 	// Al acabar, borramos la peli que hemos creado y cerramos la conexión
 	afterAll(async () => {
 		await dao.query("DELETE FROM peliculas WHERE id > 0;");
+		await dao.query("DELETE FROM usuarios WHERE id > 0;");
+		await dao.query("DELETE FROM comentarios WHERE id > 0;");
 		await pool.end();
 	});
 
 	test('Ver comentarios cuando la película existe', async () => {
-		const peli = "Alien";
+		const id = 1;
 
-		await dao.listFilms(peli).then(result => {
+		await dao.getFilmCommentaries(id).then(result => {
 			expect(result).toEqual(expect.arrayContaining([
-				expect.objectContaining({
-					nombre: "Alien: el octavo pasajero"
-				})
+				expect.objectContaining({ texto: 'Amo a mi mamá' })
 			]))
 		});		
 	});
 
 	test('Ver comentarios cuando la película no existe', async () => {
-		const peli = "peli que no existe";
+		const id = 2;
 
-		await dao.listFilms(peli).then(result => {
+		await dao.getFilmCommentaries(id).then(result => {
 			expect(result).toHaveLength(0);
 		});	
 	});
