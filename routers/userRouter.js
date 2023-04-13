2ba9f3945686254fe87728fbaa4f59208b51a206
albertomog88 <almore03@ucm.es>
2023-04-13 05:02:21 +0200

    codigo eliminado

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -19,13 +19,7 @@ const {requiresLogout} = require('../middleware/auth');
 
 userRouter.post("/signup", multerFactory.none(),
 
-    // [check("nombreCompleto","El nombre completo está vacío").notEmpty(),
-    // check("username","El nombre de usuario está vacío").notEmpty(),
-    // check("correo","Debes introducir un correo electrónico válido").isEmail(),
-    // check("password", "La longitud minima debe ser 4").isLength({ min: 4}),
-    // check("password2","La contraseña de confirmación debe tener el mismo valor que la contraseña").exists()
-    // .custom(( value, {req})=> value === req.body.password)
-    // ],
+    
     check("username", "El nombre de usuario esta vacio").notEmpty(),
     check("correo","Dirección de correo no válida o vacia").isEmail(),
     check("password", "La logintud minima debe ser 4").isLength({ min: 4}),
