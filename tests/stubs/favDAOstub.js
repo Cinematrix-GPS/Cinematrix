35b42c32000af40302be75ba2128c63e4b7c549c
andre Aylas Hernandez <oaylash@gmail.com>
2023-04-19 10:46:45 +0200

    1

--- tests/stubs/favDAOstub.js
+++ tests/stubs/favDAOstub.js
@@ -1,5 +1,18 @@
 class FavDAOStub{
+    #peliculas=[]
+    #usuarios=[]
 
+    constructor(){
+        if(typeof FavDAOStub.instance==='object')
+            return FavDAOStub.instance;
+        
+        FavDAOStub.instance=this;
+    }
+
+
+    async addFavByUser(peli,user){
+        
+    }
 
 
 
